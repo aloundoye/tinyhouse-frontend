6429bc7d0b6f50e22307968bee28b3357a703e66
Alassane Ndoye <aloutndoye.an@gmail.com>
2022-01-18 14:28:53 +0000

    Querying listings

--- src/sections/Listings/Listings.tsx
+++ src/sections/Listings/Listings.tsx
@@ -1,9 +1,34 @@
 import React from "react";
+import { server } from "../../lib/api";
 
-interface Props{
-  title : string
+const LISTINGS = `
+  query Listings{
+    listings{
+      id
+      title
+      image
+      address
+      price
+      numOfGuests
+      numOfBeds
+      rating
+    }
+  }
+`;
+interface Props {
+  title: string;
 }
 
-export const Listings = ({title}: Props) => {
-  return <h2>{title}</h2>;
+export const Listings = ({ title }: Props) => {
+  const fetchLinstings = async () => {
+    const {data} = await server.fetch({ query: LISTINGS });
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <button onClick={fetchLinstings}>Query Lisitngs</button>
+    </div>
+  );
 };
