b8109376bc422f4b36a9f27021c111a37e719aaf
Alassane Ndoye <aloutndoye.an@gmail.com>
2022-01-20 20:19:37 +0000

    add refetch option to useQuery

--- src/lib/api/useQuery.ts
+++ src/lib/api/useQuery.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { server } from "./server";
 
 interface State<TData> {
@@ -9,14 +9,17 @@ export const useQuery = <TData = any>(query: string) => {
   const [state, setState] = useState<State<TData>>({
     data: null,
   });
-
-  useEffect(() => {
+const fetch = useCallback(() => {
     const fetchApi = async () => {
-      const { data } = await server.fetch<TData>({ query });
-      setState({ data });
-    };
-    fetchApi();
-  }, [query]);
+        const { data } = await server.fetch<TData>({ query });
+        setState({ data });
+      };
+      fetchApi();
+},[query])
+  
+  useEffect(() => {
+    fetch();
+  }, [fetch]);
 
-  return state;
+  return {...state, refetch: fetch};
 };
