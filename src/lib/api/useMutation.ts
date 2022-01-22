a83e514ce9b4e24f12a9f189f2f37ed83602916a
Alassane Ndoye <aloutndoye.an@gmail.com>
2022-01-22 03:34:03 +0000

    refactoring useState with useReducer

--- src/lib/api/useMutation.ts
+++ src/lib/api/useMutation.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import { server } from "./server";
 
 interface State<TData> {
@@ -7,38 +7,61 @@ interface State<TData> {
   error: boolean;
 }
 
+type Action<TData> =
+    | { type: "FETCH" }
+    | { type: "FETCH_SUCCESS"; payload: TData }
+    | { type: "FETCH_ERROR" };
+
 type MutationTuple<TData, TVariables> = [
-  (variables?: TVariables | undefined) => Promise<void>,
-  State<TData>
+    (variables?: TVariables | undefined) => Promise<void>,
+    State<TData>
 ];
 
+const reducer =
+    <TData>() =>
+        (state: State<TData>, action: Action<TData>): State<TData> => {
+            switch (action.type) {
+                case "FETCH":
+                    return {...state, loading: true};
+                case "FETCH_SUCCESS":
+                    return {data: action.payload, loading: false, error: false};
+                case "FETCH_ERROR":
+                    return {...state, loading: false, error: true};
+                default:
+                    throw new Error();
+            }
+        };
+
 export const useMutation = <TData = any, TVariables = any>(
-  query: string
+    query: string
 ): MutationTuple<TData, TVariables> => {
-  const [state, setState] = useState<State<TData>>({
-    data: null,
-    loading: false,
-    error: false,
-  });
+    const fetchReducer = reducer<TData>();
 
-  const fetch = async (variables?: TVariables) => {
-    try {
-      setState({ data: null, loading: true, error: false });
+    const [state, dispatch] = useReducer(fetchReducer, {
+        data: null,
+        loading: false,
+        error: false,
+    });
 
-      const { data, errors } = await server.fetch<TData, TVariables>({
-        query,
-        variables,
-      });
 
-      if (errors && errors.length) {
-        throw new Error(errors[0].message);
-      }
+    const fetch = async (variables?: TVariables) => {
+        try {
+            dispatch({type: "FETCH"});
 
-      setState({ data: data, loading: false, error: false });
-    } catch (err) {
-      setState({ data: null, loading: false, error: true });
-    }
-  };
+            const {data, errors} = await server.fetch<TData, TVariables>({
+                query,
+                variables,
+            });
 
-  return [fetch, state];
+            if (errors && errors.length) {
+                throw new Error(errors[0].message);
+            }
+
+            dispatch({type: "FETCH_SUCCESS", payload: data});
+        } catch (err) {
+            dispatch({type: "FETCH_ERROR"});
+        }
+    };
+
+    return [fetch, state];
 };
--- src/sections/Listings/Listings.tsx
+++ src/sections/Listings/Listings.tsx
@@ -34,34 +34,34 @@ interface Props {
   title: string;
 }
 
-export const Listings = ({ title }: Props) => {
-  const { data, loading, error, refetch } = useQuery<ListingsData>(LISTINGS);
+export const Listings = ({title}: Props) => {
+  const {data, loading, error, refetch} = useQuery<ListingsData>(LISTINGS);
 
   const [
     deleteListing,
-    { loading: deleteListingLoading, error: deleteListingError },
+    {loading: deleteListingLoading, error: deleteListingError},
   ] = useMutation<DeleteListingData, DeleteListingVariables>(DELETE_LISTING);
 
   const handleDeleteListing = async (id: string) => {
-    await deleteListing({ id });
+    await deleteListing({id});
     refetch();
   };
 
   const listings = data ? data.listings : null;
 
   const listingsList = listings ? (
-    <ul>
-      {listings.map((listing) => {
-        return (
-          <li key={listing.id}>
-            {listing.title}{" "}
-            <button onClick={() => handleDeleteListing(listing.id)}>
-              X delete
-            </button>
-          </li>
-        );
-      })}
-    </ul>
+      <ul>
+        {listings.map((listing) => {
+          return (
+              <li key={listing.id}>
+                {listing.title}{" "}
+                <button onClick={() => handleDeleteListing(listing.id)}>
+                  X delete
+                </button>
+              </li>
+          );
+        })}
+      </ul>
   ) : null;
 
   if (loading) {
@@ -73,21 +73,21 @@ export const Listings = ({ title }: Props) => {
   }
 
   const deleteListingLoadingMessage = deleteListingLoading ? (
-    <h4>Deletion in progress...</h4>
+      <h4>Deletion in progress...</h4>
   ) : null;
 
   const deleteListingErrorMessage = deleteListingError ? (
-    <h4>
-      Uh oh! Something went wrong with deleting - please try again later :(
-    </h4>
+      <h4>
+        Uh oh! Something went wrong with deleting - please try again later :(
+      </h4>
   ) : null;
 
   return (
-    <div>
-      <h2>{title}</h2>
-      {listingsList}
-      {deleteListingLoadingMessage}
-      {deleteListingErrorMessage}
-    </div>
+      <div>
+        <h2>{title}</h2>
+        {listingsList}
+        {deleteListingLoadingMessage}
+        {deleteListingErrorMessage}
+      </div>
   );
 };
